b8704d93b4df38a928f3b98952a804097533e04b
NahuelRc10 <nahueltabasso@gmail.com>
2021-08-04 22:08:20 -0300

    Se implemento el menu en el sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,24 +2,79 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+// ANGULAR MATERIAL
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatChipsModule } from '@angular/material/chips';
+
 import { NavbarHeaderComponent } from './navbar-header/navbar-header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { TitlePageComponent } from './title-page/title-page.component';
 
 @NgModule({
   declarations: [
     NavbarHeaderComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    TitlePageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatCardModule,
+    MatAutocompleteModule,
+    MatTabsModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatStepperModule,
+    MatChipsModule
   ],
   exports: [
     NavbarHeaderComponent,
     SidebarComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
+    FooterComponent,
+    TitlePageComponent,
+    MatPaginatorModule,
+    MatTableModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatCardModule,
+    MatAutocompleteModule,
+    MatTabsModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatStepperModule,
+    MatChipsModule
   ]
 })
 export class SharedModule { }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UsuarioResponseDTO } from 'src/app/models/response.model';
 import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,15 +11,38 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class SidebarComponent implements OnInit {
 
+  menuItems: any[] = [];
+  imgUrl: string = '';
+  usuario: UsuarioResponseDTO = new UsuarioResponseDTO();
+  url: string = environment.server_url + '/file';
+
   constructor(private authService: AuthService,
               private router: Router) {}
 
   ngOnInit(): void {
+    this.usuario = this.authService.usuario
+    this.imgUrl = this.getUrlImagen();
+    this.authService.getMenu(this.usuario.rol.nombreRol).subscribe((data: any) => {
+      this.menuItems = data.menu;
+    });
   }
 
   public logout() {
     this.authService.logout();
     this.router.navigateByUrl('/login');
+  }
+
+  private getUrlImagen() {
+    if (this.usuario.google) {
+      if (this.usuario.srcImagen.includes('https')) {
+        return this.usuario.srcImagen;
+      }
+    }
+
+    if (!this.usuario.srcImagen) {
+        return `${this.url}/perfiles/no-image.png`;
+    } 
+    return `${this.url}/perfiles/${this.usuario.srcImagen}`;
   }
 
 }
