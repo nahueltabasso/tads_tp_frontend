317249df9d87a27931b73b0c83aac53ebaa38bff
unknown <fernandoo@gmail.com>
2022-02-18 19:49:02 -0300

    Prueba eliminar usuarios

--- src/app/pages/mantenedores/usuarios-mantenedor/usuarios-mantenedor.component.ts
+++ src/app/pages/mantenedores/usuarios-mantenedor/usuarios-mantenedor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioResponseDTO } from 'src/app/models/response.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-usuarios-mantenedor',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosMantenedorComponent implements OnInit {
 
-  constructor() { }
+  usuarioLogueado: UsuarioResponseDTO = new UsuarioResponseDTO();
+  usuarios: UsuarioResponseDTO[] = [];
+  flagNoResults: boolean = false;
+  flagLoading: boolean = false;
+
+  constructor(private usuarioService: UsuarioService,
+    private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.usuarioLogueado = this.authService.usuario;
+    this.flagLoading = true;
+    this.usuarioService.getAll().subscribe((data: any) => {
+      this.usuarios = data.usuarios;
+      console.log("USUARIOS", this.usuarios);
+      if (this.usuarios.length === 0) {
+        this.flagNoResults = true;
+        this.flagLoading = false;
+        return;
+      }
+      this.flagNoResults = false;
+      this.usuarios = this.usuarios.filter(a => a.id !== this.usuarioLogueado.id);
+      this.flagLoading = false;
+    });
+  }
+
+  public getImagen(u: UsuarioResponseDTO) {
+    return this.usuarioService.getUrlImagen(u);
+  }
+
+  public deleteUsuario(id: string) {
+    console.log("Entra a deleteUsuario ID=", id);
+    this.usuarioService.deleteUsuarioById(id).subscribe((data: any) => {
+      console.log(data);
+    }, (err) => {console.log("ERR", err)});
   }
 
 }
